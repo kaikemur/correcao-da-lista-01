4d5f1fceccc0b9837f40a67bea408799e1321c91
kaike <kaike.ribeiro@aluno.senai.br>
2025-05-19 09:40:33 -0300

    feat: finalização da questao02

--- scripts/questao-02.js
+++ scripts/questao-02.js
@@ -1,1 +1,22 @@
-const números = [12, 5, 8, 21, 16, 7, 30, 45, 13, 27]
+const numeros = [12, 5, 8, 21, 16, 7, 30, 45, 13, 27];
+let numerosPares = 0;
+let numerosImpares = 0;
+let maioresQue20 = 0;
+
+//Varredura do array
+for (let i = 0; i < numeros .length; i++) {
+    //condicional para numeros pares
+    if(numeros[i] % 2 ==0){
+    numerosPares++ ;
+    } else {
+         numerosImpares++;
+    }
+    //verificação para maiores que 20
+    if(numeros[i]>20 ){
+    maioresQue20++
+}
+}
+console.log(`A quantidade de números pares é: ${numerosPares}`);
+console.log(`A quantidade de números impares é: ${numerosImpares}`);
+console.log(`A quantidade de números maiores que 20 é: ${maioresQue20}`)
+
