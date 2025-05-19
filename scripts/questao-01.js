f72f72cec564533988c674022d3e2c22d5801c42
kaike <kaike.ribeiro@aluno.senai.br>
2025-05-19 09:06:56 -0300

    feat: finaliza resolução da questão 01

--- scripts/questao-01.js
+++ scripts/questao-01.js
@@ -1,1 +1,15 @@
+const idades=[17,21,16,25,19];
+let maioresDeidade = 0;
+let menoresDeidade = 0;
+//e um laço de repetição para varrer todos os elementos do meu array idades
+for (let i = 0; i < idades.length; i++) {
+    //condição para contadores de maior e menor idade
+    if(idades[i]>=18){
+        maioresDeidade ++;
+    }
 
+}
+
+menoresDeidade=idades.length - maioresDeidade
+console.log(`A quantidade de pessoas maiores de idades é: ${maioresDeidade}`);
+console.log(`A quantidade de pessoas menores menores de idade é: ${menoresDeidade}`);
